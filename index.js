01f15ead2ea966e4c8d60acc37e057e0ae61ce42
Delanie Perez <delanieperez11@gmail.com>
2018-05-23 01:25:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,12 +10,35 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*100);
+  var newItem = {
+    itemName: item,
+    itemPrice: price,
+  };
+  cart.push(newItem);
+  return `${newItem.itemName} has been added to your cart.`;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0) {
+    return "Your shopping cart is empty.";
+  } else {
+    
+    var inCart = 'In your cart, you have';
+    
+    for (var i = 0; i < cart.length; i++){
+      if(cart.length === 1){
+        inCart += ` ${getCart()[i].itemName} at $${getCart()[i].itemPrice}.`;
+    } else if(cart.length === 2){
+       inCart += ` ${getCart()[i].itemName} at $${getCart()[i].itemPrice}, and ${getCart()[i].itemName} at $${getCart()[i].itemPrice}.`;
+      } else {
+        inCart += ``;
+      }
+    }
+    return inCart;
+  }
 }
+
 
 function total() {
   // write your code here
